d7c15fe71f4351d04eeb7ca6bb3ee7835c650d0f
orazz <orazz.tm@gmail.com>
2020-07-23 23:26:10 +0300

    remove unnecessary files

---
+++ Task_Teqqed/Resources/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Task_Teqqed
+//
+//  Created by Oraz Atakishiyev on 7/21/20.
+//  Copyright © 2020 orazz. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
+
